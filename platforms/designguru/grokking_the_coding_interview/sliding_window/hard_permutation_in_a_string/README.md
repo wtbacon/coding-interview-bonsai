bead20a338b226021ce4699d921f080ea2408727
wtbacon <wtbaaaacon@gmail.com>
2026-01-10 07:12:47 +0800

    fix: update the solution

--- platforms/designguru/grokking_the_coding_interview/sliding_window/hard_permutation_in_a_string/README.md
+++ platforms/designguru/grokking_the_coding_interview/sliding_window/hard_permutation_in_a_string/README.md
@@ -10,9 +10,10 @@ Given a string and a pattern, return `True` if the string contains any permutati
 
 - Time: $O(N)$
 - Space: $O(1)$
+  - $O(1)$ assuming a fixed character set (like 26 letters).
 - Approach:
-  1. Iterate through the string using a sliding window.
-  2. During the iteration, maintain a running count of character frequencies using a hashmap.
-  3. Decrement the count for characters entering the window and increment for those leaving.
-  4. If a character is not found in the pattern, reest of shift the window past that index.
-  5. Return `True` when all frequency counts reach zero.
+  1. Iterate through the string using a fixed-size sliding window.
+  2. Count the frequencies of each character in the pattern using a hashmap.
+  3. During the iteration, increment the `matched` counter when the frequency target is met.
+  4. Decrement the `matched` counter when the condition is no longer met.
+  5. Return `True` when the `matched` counter equals the number of unique characters in the pattern.
--- platforms/designguru/grokking_the_coding_interview/sliding_window/hard_permutation_in_a_string/solution.py
+++ platforms/designguru/grokking_the_coding_interview/sliding_window/hard_permutation_in_a_string/solution.py
@@ -4,22 +4,30 @@ from collections import defaultdict
 class Solution:
     def findPermutation(self, str1: str, pattern: str):
         char_freq_in_ptn = defaultdict(int)
-        for r in pattern:
-            char_freq_in_ptn[r] += 1
+        for c in pattern:
+            char_freq_in_ptn[c] += 1
 
-        left = 0
+        # Only increment 'matched' when the window's character count equals the pattern's required count
+        # because you only increment it once per unique character when the frequency target is met.
+        left, matched = 0, 0
         for right in range(len(str1)):
-            r = str1[right]
-            while left < right and r not in char_freq_in_ptn:
-                l = str1[left]
-                if l in pattern:
-                    char_freq_in_ptn[l] += 1
+            right_char = str1[right]
+            if right_char in char_freq_in_ptn:
+                char_freq_in_ptn[right_char] -= 1
+                if char_freq_in_ptn[right_char] == 0:
+                    matched += 1
+
+            if len(char_freq_in_ptn) == matched:
+                return True
+
+            if right >= len(pattern) - 1:
+                left_char = str1[left]
+                if left_char in char_freq_in_ptn:
+                    if char_freq_in_ptn[left_char] == 0:
+                        matched -= 1
+                    # Need to increment after check whether the 'left_char' frequency is 0
+                    # because you can't know whether it matched when the pattern contains duplicated characters.
+                    char_freq_in_ptn[left_char] += 1
                 left += 1
-            if r in char_freq_in_ptn:
-                char_freq_in_ptn[r] -= 1
-                if char_freq_in_ptn[r] == 0:
-                    del char_freq_in_ptn[r]
-                if len(char_freq_in_ptn) == 0:
-                    return True
 
         return False
